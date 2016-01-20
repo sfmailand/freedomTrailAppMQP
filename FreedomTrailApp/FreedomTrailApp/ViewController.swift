00d94349f6872079904abdd08eb7392c06f168dd
Sam Mailand <sam.mailand@gmail.com>
2016-01-20 13:17:33 -0500

    First Commit

---
+++ FreedomTrailApp/FreedomTrailApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FreedomTrailApp
+//
+//  Created by Sam Mailand on 1/20/16.
+//  Copyright © 2016 Sam Mailand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
