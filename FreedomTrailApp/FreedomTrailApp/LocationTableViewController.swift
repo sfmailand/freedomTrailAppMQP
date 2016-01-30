ba973c8498f1509921cbf64d87e4a620896f12c9
Sam Mailand <sam.mailand@gmail.com>
2016-01-30 11:38:31 -0500

    Table View Navigation
    Clicking a location in the table now navigates to another page, which will eventually have the information for the location. This page currently just displays the name.

--- FreedomTrailApp/FreedomTrailApp/LocationTableViewController.swift
+++ FreedomTrailApp/FreedomTrailApp/LocationTableViewController.swift
@@ -113,14 +113,17 @@ class LocationTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let mealDetailViewController = segue.destinationViewController as! LocationDetailViewController
+        
+        if let selectedLocationCell = sender as? LocationTableViewCell{
+            let indexPath = tableView.indexPathForCell(selectedLocationCell)!
+            let selectedLocation = locations[indexPath.row]
+            mealDetailViewController.location = selectedLocation
+        }
     }
-    */
 
 }
