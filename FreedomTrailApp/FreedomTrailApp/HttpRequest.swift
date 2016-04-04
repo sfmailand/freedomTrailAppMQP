b43e687d71d27bbc1115197555e6c6f4b7a2e7ff
Sam Mailand <sam.mailand@gmail.com>
2016-04-04 14:16:17 -0400

    Added class for making http get requests

---
+++ FreedomTrailApp/FreedomTrailApp/HttpRequest.swift
@@ -0,0 +1,70 @@
+//
+//  httpRequest.swift
+//  FreedomTrailApp
+//
+//  Created by Sam Mailand on 4/4/16.
+//  Copyright © 2016 Sam Mailand. All rights reserved.
+//
+
+import Foundation
+
+
+public class HttpRequest{
+    
+    
+    init?(){
+    }
+    
+    
+    public func getRequest(requestString: String){
+        
+        // Send HTTP GET Request
+        
+        // Create NSURL Ibject
+        let myUrl = NSURL(string: requestString);
+        
+        // Creaste URL Request
+        let request = NSMutableURLRequest(URL:myUrl!);
+        
+        // Set request HTTP method to GET. It could be POST as well
+        request.HTTPMethod = "GET"
+
+
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            // Check for error
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response string
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("responseString = \(responseString)")
+            
+            
+            // Convert server json response to NSDictionary
+            do {
+                if let convertedJson = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
+                    
+                    // Print out dictionary
+//                    print(convertedJson)
+//                    
+//                    // Get value by key
+//                    let firstNameValue = convertedJson["userName"] as? String
+//                    print(firstNameValue!)
+                    
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+        }
+        
+        task.resume()
+        
+        
+    }
+}
